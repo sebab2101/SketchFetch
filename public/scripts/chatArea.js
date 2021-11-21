5072d29158c782c54c23db122f744f48bf01c41e
DevAkre <akre1@kenyon.edu>
2021-11-21 16:57:21 -0500

    adding stuf chat

--- public/scripts/chatArea.js
+++ public/scripts/chatArea.js
@@ -8,5 +8,23 @@ class chatArea{
         chatMessages = chatBox.querySelector("#");
         chatInput = chatBox.querySelector("#");
         chatForm = chatBox.querySelector("#");
+        this.addListeners();
     }
+
+    addMessage(msg){
+        var item = document.createElement('li');
+        item.textContent = msg;
+        this.chatMessages.appendChild(item);
+        // window.scrollTo(0, document.body.scrollHeight);
+    }
+    addListeners(){
+        this.chatF.addEventListener('submit', function(e) {
+            e.preventDefault();
+            if (input.value) {
+              socket.emit('chat message', input.value);
+              input.value = '';
+            }
+        });
+    }
+    
 }
