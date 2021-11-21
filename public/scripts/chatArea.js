cf0fbaddf6eefe2654cba0676a067c5b829ba972
DevAkre <akre1@kenyon.edu>
2021-11-21 15:24:23 -0500

    chatBox stuff

---
+++ public/scripts/chatArea.js
@@ -0,0 +1,12 @@
+class chatArea{
+    chatBox;
+    chatMessages;
+    chatForm;
+    chatInput;
+    constructor(){
+        chatBox = document.querySelector("#chatBox");
+        chatMessages = chatBox.querySelector("#");
+        chatInput = chatBox.querySelector("#");
+        chatForm = chatBox.querySelector("#");
+    }
+}
